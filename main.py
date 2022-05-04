8de3db0bc36790ea66c6469a9198107acb4bdfda
Zappricious <97460714+Zappricious@users.noreply.github.com>
2022-05-04 13:06:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
+#Hi Guys Zappricious Here!
+#This is a program that auto-grinds coins
+
 import pyautogui
 import time
+import random
+
+time.sleep(5)
+
+default_cl = 2
+
+clickx,clicky = 503,887
+
+rand_item = ['shovel', 'rifle', 'fishing', 'lifesaver', 'lucky']
+
+
+def write(string, cooldown):
+    pyautogui.write(string)
+    pyautogui.press('enter')
+    time.sleep(cooldown)
+
 
 def basic_cmd():
-    pyautogui.write('pls use lucky')
-    pyautogui.press('enter')
-    pyautogui.write('pls beg')
-    pyautogui.press('enter')
-    pyautogui.write('pls dig')
-    pyautogui.press('enter')
-    pyautogui.write('pls fish')
-    pyautogui.press('enter')
-    pyautogui.write('pls hunt')
-    pyautogui.press('enter')
-    pyautogui.write('pls dep max')
-    pyautogui.press('enter')
+    write('----------------------THIS IS A PROGRAM RUNNING--------------------', default_cl)
+    write('pls use lucky', default_cl)
+    write('pls beg', default_cl)
+    write('pls dig', default_cl)
+    write('pls fish', default_cl)
+    write('pls hunt', default_cl)
+    write('pls pm', default_cl)
+    pyautogui.click(847,882)
+    time.sleep(1)
+
+    write('pls search', default_cl)
+    pyautogui.click(clickx,clicky)
+    time.sleep(1)
+
+    write('pls crime', default_cl)
+    pyautogui.click(clickx,clicky)
+    time.sleep(1)
+
+    write('pls dep max', default_cl)
 
 
-    time.sleep(45)
+    to_buy = random.choice(rand_item)
+    write(f'pls buy {to_buy}', 0)
 
 while True:
-    time.sleep(5)
     basic_cmd()
+    time.sleep(15)
+
+
+
 
 
