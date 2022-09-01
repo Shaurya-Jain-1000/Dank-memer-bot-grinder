fb1819c81ee10cea0fcbbfe752169d1b44aa2afa
Zappricious <97460714+Zappricious@users.noreply.github.com>
2022-09-01 09:58:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,121 @@
-#Hi Guys Zappricious Here!
-#This is a program that auto-grinds coins
+# Hi Guys Zappricious Here!
+# This is a program that auto-grinds coins
 
 import pyautogui
 import time
 import random
+import schedule
+import keyboard
 
 time.sleep(5)
 
-default_cl = 2
+command_cooldown = 0.2
 
-clickx,clicky = 503,887
+default_cl = 2
+default_speed = 0.1
+clickx, clicky = 503, 887
+
+
 
 rand_item = ['shovel', 'rifle', 'fishing', 'lifesaver', 'lucky']
 
 
-def write(string, cooldown):
-    pyautogui.write(string)
+
+def write(string, cooldown=default_cl):
+    pyautogui.write(string, interval = default_speed )
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
     pyautogui.press('enter')
     time.sleep(cooldown)
 
+def use(item, cooldown=default_cl):
+    pyautogui.write('/use', interval=default_speed)
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
+    pyautogui.write(item, interval=default_speed)
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(default_cl)
+
+def run_2(command, input, cooldown=default_cl):
+    pyautogui.write(f'/{command}', interval=default_speed)
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
+    pyautogui.write(input, interval=default_speed)
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(default_cl)
+
+def depmax(cooldown=default_cl):
+    pyautogui.write('/deposit', interval=default_speed)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
+    pyautogui.write('max', interval=default_speed)
+    pyautogui.press('enter')
+    time.sleep(command_cooldown)
+    pyautogui.press('enter')
+    time.sleep(default_cl)
+
+def click(x, y, cooldwon=default_cl):
+    pyautogui.click(x, y)
+    time.sleep(cooldwon)
+
+
+def stream():
+    write('/stream')
+    click(772, 841, 1.5)
+    click(695, 892, 1.5)
+    click(720, 737, 1.5)
+
+def work():
+    write('/work')
+    write('/use tipjar')
+
+def start():
+    use('apple')
+    depmax()
+    use('pizza')
+    time.sleep(3)
+    use('lucky')
 
 def basic_cmd():
-    write('----------------------THIS IS A PROGRAM RUNNING--------------------', default_cl)
-    write('pls use lucky', default_cl)
-    write('pls beg', default_cl)
-    write('pls dig', default_cl)
-    write('pls fish', default_cl)
-    write('pls hunt', default_cl)
-    write('pls pm', default_cl)
-    pyautogui.click(847,882)
-    time.sleep(1)
-
-    write('pls search', default_cl)
-    pyautogui.click(clickx,clicky)
-    time.sleep(1)
-
-    write('pls crime', default_cl)
-    pyautogui.click(clickx,clicky)
-    time.sleep(1)
-
-    write('pls dep max', default_cl)
-
-
+    write('/beg')
+    write('/dig')
+    write('/fish')
+    write('/hunt')
+    write('/postmemes')
+    click(847, 882, 1)
+    write('/search')
+    click(clickx, clicky, 1)
+    write('/crime')
+    click(clickx, clicky, 1)
+    write('/highlow')
+    click(clickx, clicky, 1)
+    run_2('gamble', '1500')
+    depmax()
     to_buy = random.choice(rand_item)
-    write(f'pls buy {to_buy}', 0)
+    # write(f'/buy {to_buy}', 0)
 
-while True:
+# schedule.every(60).minutes.do(work)
+schedule.every(125).minutes.do(use,'pizza')
+schedule.every(33).minutes.do(use,'lucky')
+# stream()
+# schedule.every(10).minutes.do(stream)
+start()
+running = True
+while running:
     basic_cmd()
-    time.sleep(15)
+    schedule.run_pending()
+    click(953,649)
+    time.sleep(7)
+
 
 
 
